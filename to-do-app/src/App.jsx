55c640053c5a562c0c2e77299396795245f41e1f
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-01-23 21:57:20 +0530

    commit

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+
+function generateId(){
+  return Math.floor(Math.random()*100);
+}
+
+
+function App() {
+  const [todos,setTodos]=useState([]);
+  const [input,setinput]=useState('');
+
+  const handleSubmit=()=>{
+    setTodos(todos=>
+      todos.concat({
+        text:input,
+        id:generateId()
+      })
+    )
+      setinput("");
+    
+  }
+  const removeTodo=(id)=>{
+    setTodos((todos)=>todos.filter((t)=>t.id!==id))
+  }
+ 
+
+  return (
+    <>
+    
+    <div className=" mx-auto mt-[100px] w-[500px] h-[300px]  place-items-center flex flex-col items-center ">
+        <h1 className="mt-5 font-bold text-3xl">Todo List</h1>
+        <div className="flex gap-4 m-8">
+        <input className=" border border-black p-2 rounded-md" type="text" value={input} onChange={(e)=>setinput(e.target.value)}
+        placeholder="New Todo"/>
+
+        <button className="border border-none p-2 rounded-md bg-green-400 hover:bg-green-600,border-black cursor-pointer" onClick={handleSubmit}>Add</button>
+        </div>
+        
+        <ul>
+          {todos.map(({text,id})=>{
+            return( 
+            <li className="bg-violet-100 m-2 place-items-center border-none w-[260px] p-2 flow-root rounded-md" key={id}>
+              <span className="float-left">{text}</span>
+              <button className=" w-6 h-6 place-items-center bg-red-600 float-right rounded-md" onClick={()=>removeTodo(id)}>X</button>
+            </li>
+            ) 
+
+          })}
+        </ul>
+        
+      </div>
+        
+    </>
+  )
+}
+
+export default App
