7188794f90b791a646b38711034e33b7334fa3d4
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-01-24 18:59:57 +0530

    commit

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -7,20 +7,47 @@ function generateId(){
 
 function App() {
   const [todos,setTodos]=useState([]);
-  const [input,setinput]=useState('');
+  const [input,setInput]=useState('');
+  const [editId,setEditId]=useState(0);//contains the id of the lelement that we should edit
 
-  const handleSubmit=()=>{
-    setTodos(todos=>
-      todos.concat({
-        text:input,
-        id:generateId()
-      })
-    )
-      setinput("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(editId){
+      const editTodo=todos.find((i)=>i.id===editId);
+      const updatedTodos=todos.map((t)=>t.id===editTodo.id 
+      ? (t={id:t.id,text:input})
+      : {id:t.id,text:t.text}
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setInput("");
+      return;
+    }
+
+    if(input!==''){
+      setTodos(todos=>
+        todos.concat({
+          text:input,
+          id:generateId()
+        })
+      )
+
+    }
+    setInput("");
     
   }
   const removeTodo=(id)=>{
     setTodos((todos)=>todos.filter((t)=>t.id!==id))
+  }
+//1.whenever you click on edit the text should be wriiten in text box 
+//2.then the button should be edit instead of add for this we need another state to tell us if it is in edit mode or not
+//3.now the final step is to reflect the edited text in to the array when we click on the edit button 
+  const handleEdit=(id)=>{
+    const editTodo=todos.find((i)=>i.id===id);
+    setInput(editTodo.text);
+    setEditId(id);
+
+
   }
  
 
@@ -30,10 +57,10 @@ function App() {
     <div className=" mx-auto mt-[100px] w-[500px] h-auto place-items-center flex flex-col items-center bg-purple-200 p-6 rounded-lg">
         <h1 className="mt-5 font-bold text-3xl">Todo List</h1>
         <div className="flex gap-4 m-8 ">
-        <input className=" border border-black p-2 rounded-md outline-none w-[300px]" type="text" value={input} onChange={(e)=>setinput(e.target.value)}
+        <input className=" border border-black p-2 rounded-md outline-none w-[300px]" type="text" value={input} onChange={(e)=>setInput(e.target.value)}
         placeholder="New Todo"/>
 
-        <button className="border border-none p-2 rounded-md bg-green-300 hover:bg-green-600 cursor-pointer " onClick={handleSubmit}>Add</button>
+        <button className="border border-none p-2 rounded-md bg-green-300 hover:bg-green-600 cursor-pointer " onClick={handleSubmit}>{editId ? "Edit" : "Add"}</button>
         </div>
         
         <ul>
@@ -41,7 +68,10 @@ function App() {
             return( 
             <li className="bg-violet-100 m-2 mb-2 place-items-center border-none w-[360px] p-2 flow-root rounded-md" key={id}>
               <span className="float-left">{text}</span>
-              <button className=" w-6 h-6 place-items-center bg-red-600 float-right rounded-md" onClick={()=>removeTodo(id)}>X</button>
+              <div className="float-right flex  gap-2">
+              <button className=" w-10 h-7 place-items-center bg-slate-400  rounded-md " onClick={()=>handleEdit(id)}>Edit</button>
+              <button className=" w-6 h-7 place-items-center bg-red-600  rounded-md" onClick={()=>removeTodo(id)}>X</button>
+              </div>
             </li>
             ) 
 
