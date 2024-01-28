32ed7005da85a6fbcb0d73c8fd65629a21e218bb
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-01-28 22:34:29 +0530

    commit

---
+++ hidden-search-bar/src/App.jsx
@@ -0,0 +1,70 @@
+// import {useState} from 'react';
+// import {FaSearch} from 'react-icons/fa';
+// import './App.css';
+// function App() {
+//   const [showInput,setShowInput]=useState(false);
+//   const [bgColor,setBgColor]=useState("white");
+
+//   const handleClick=(e)=>{
+//     setBgColor('#1a1a1a');
+//     if(e.target.className==="container"){
+//       setShowInput(false);
+//       setBgColor("#fff");
+//     }
+
+
+//   }
+ 
+
+//   return (
+//     <>
+//     <section className="container" style={{backgroundColor:bgColor}}
+//     onClick={handleClick}
+//     >
+
+//       {showInput?(
+//         <input type="text" placeholder="Search..."/>
+//       ):(
+//         <FaSearch onClick={()=>setShowInput(true)}/>
+//       )}
+
+//     </section>
+      
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState("white");
+
+  const handleClick = (e) => {
+    setBgColor("#1a1a1a");
+    if (e.target.className === "container") {
+      setShowInput(false);
+      setBgColor("#fff");
+    }
+  };
+
+  return (
+    <section
+      className="container"
+      style={{ backgroundColor: bgColor }}
+      onClick={handleClick}
+    >
+      {showInput ? (
+        <input type="text" placeholder="Search..." />
+      ) : (
+        <FaSearch onClick={() => setShowInput(true)} />
+      )}
+    </section>
+  );
+}
+
+export default App;
