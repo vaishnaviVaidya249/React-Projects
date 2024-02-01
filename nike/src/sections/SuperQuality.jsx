5f5cbce6929e39f314dd5b571b866b3b15ae6842
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-02-01 21:43:08 +0530

    commit

---
+++ nike/src/sections/SuperQuality.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SuperQuality = () => {
+  return (
+    <section id="about-us" className="flex justify-between
+    items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2></h2>
+      </div>
+    </section>
+  )
+}
+
+export default SuperQuality
