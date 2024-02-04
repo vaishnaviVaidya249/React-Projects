5ddde65fa4d8f287580e80a0dc0e771723d5270e
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-02-04 13:08:56 +0530

    commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,16 @@
+import TopButtons from './components/TopButtons.jsx';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import Inputs from './components/Inputs.jsx';
+function App() {
+  
+
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+       <TopButtons/>
+       <Inputs/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ weather-app/src/Inputs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inputs = () => {
+  return (
+    <div>
+      Inputs
+    </div>
+  )
+}
+
+export default Inputs
