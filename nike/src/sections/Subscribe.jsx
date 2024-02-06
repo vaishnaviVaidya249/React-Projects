9ffe4a210eea7f0c1e1edbae1cc3473eae239b53
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-02-06 17:12:54 +0530

    commit

--- nike/src/sections/Subscribe.jsx
+++ nike/src/sections/Subscribe.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Subscribe = () => {
   return (
-    <div>
-      
-    </div>
+   <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+    <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from
+      <span className="text-coral-red"> 
+      Updates </span> & Newsletter
+
+    </h3>
+
+   </section>
   )
 }
 
